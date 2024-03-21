b9319709508462da085190164232ce65d5754e91
Konata <shinoyashirosama@gmail.com>
2024-03-21 14:47:46 +0800

    maven

--- src/main/java/string/LengthDemo.java
+++ src/main/java/string/LengthDemo.java
@@ -1,4 +1,9 @@
 package string;
 
 public class LengthDemo {
+    public static void main(String[] args) {
+        String Line = "我爱java!";
+        int len = Line.length();
+        System.out.printf("Len:"+len);
+    }
 }
