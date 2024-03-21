41c33a3ce890e8177a94c322d1bd8148dd58e7d6
Konata <shinoyashirosama@gmail.com>
2024-03-21 17:48:12 +0800

    maven

--- src/main/java/string/toUpperCase.java
+++ src/main/java/string/toUpperCase.java
@@ -1,4 +1,7 @@
 package string;
+
+import java.util.Scanner;
+
 //to Upper/Lower case将数组中的英文转为大写/小写
 public class toUpperCase {
     public static void main(String[] args) {
@@ -7,6 +10,20 @@ public class toUpperCase {
         System.out.println(upper);
         String lower = str.toLowerCase();
         System.out.println(lower);
+
+        String code = "A78dsB";
+        System.out.println("请输入验证码"+code);
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        code = code.toLowerCase();
+        input = code.toLowerCase();
+        System.out.println(code.equals(input));
+
+
+
+
+
 
     }
 }
