6ed79c83fdb2f695c6fa8d5c30a786bdab4bbd73
Konata <shinoyashirosama@gmail.com>
2024-03-21 20:47:11 +0800

    maven

---
+++ src/main/java/day01/Test02.java
@@ -0,0 +1,32 @@
+package day01;
+
+import java.util.Scanner;
+
+/**
+ * 要求用户在控制台输入自己的用户名。
+ * 然后要求做如下验证工作:
+ * 1:用户名不能为空(只要有一个字符)
+ * 2:用户名必须在20个字以内
+ * @author Xiloer
+ *
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            String input = scan.nextLine();
+            if(input.length()==0){
+                System.out.println("用户名不能为空");
+
+
+            } else if (input.length()>20) {
+                System.out.println("用户名必须在20个字以内");
+
+            }else return;
+        }
+
+
+    }
+
+
+}
