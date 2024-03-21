211138883759b8f8384773586dbf6a43cf0d8c5f
Konata <shinoyashirosama@gmail.com>
2024-03-21 19:43:18 +0800

    maven

---
+++ src/main/java/string/ValueOfDemo.java
@@ -0,0 +1,22 @@
+package string;
+
+public class ValueOfDemo {
+    public static void main(String[] args) {
+        int a = 123;
+        double b = 123.123;
+
+        String stra =String.valueOf(a);
+        System.out.println(stra);
+        String strb = String.valueOf(b);
+        System.out.println(b);
+
+        String a1 = a+"";
+        System.out.println(a1);
+
+
+
+    }
+
+
+
+}
