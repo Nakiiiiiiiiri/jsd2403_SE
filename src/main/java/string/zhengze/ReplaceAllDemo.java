bbdbb462123004ebbe4647b80bb0bd10ee2398d0
Konata <shinoyashirosama@gmail.com>
2024-03-22 15:20:14 +0800

    maven

--- src/main/java/string/zhengze/ReplaceAllDemo.java
+++ src/main/java/string/zhengze/ReplaceAllDemo.java
@@ -5,5 +5,12 @@ public class ReplaceAllDemo {
         String line = "abc123cde123fgh897";
         String newline = line.replaceAll("[0-9]+","NUMBER");
         System.out.println(newline);
+
+        String regex = "(nmlgb|nc|sb)";
+        String message = "nmlgb,你是nc吧，sb";
+        String newmessage = message.replaceAll(regex,"***");
+        System.out.println(newmessage);
+
+
     }
 }
