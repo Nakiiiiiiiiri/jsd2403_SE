ef4f9228aa1a8833bb9922790fa4e5db4ddb7a38
Konata <shinoyashirosama@gmail.com>
2024-03-28 16:33:59 +0800

    更新

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,20 +1,44 @@
 package socket;
 
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 //Socket封装了TCP协议的通讯细节，使用他可以喝远端计算机建立网络连接，并基于两条流支持用户通讯
 public class Client {
     private Socket socket;
     public Client(){
         try{
             System.out.println("正在连接诶服务端");
-            socket = new Socket("127.0.0.1",8088);
+            socket = new Socket("176.17.13.62",8088);
             //socket = new Socket("localhost",8088);//127.0.0.1是本机地址也可以写成localhost
             System.out.println("与服务端成功连接");
-        }catch (Exception e){
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
-    public void start(){}
+    public void start(){
+        try{
+            OutputStream out  = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter(bw,true);//自动行刷新
+            out.write(1);
+            while (true){
+                Scanner scan = new Scanner(System.in);
+                String line = scan.nextLine();
+                pw.println(line);
+                if(line.equalsIgnoreCase("bye")){
+                    break;
+                }
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
         Client client = new Client();
--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -1,4 +1,54 @@
 package socket;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+//聊天室服务端
 public class Server {
+    private ServerSocket serverSocket;
+    public Server(){
+        try{
+            System.out.println("正在启动服务端");
+            serverSocket = new ServerSocket(8088);
+            System.out.println("服务端启动完毕");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void start(){
+        try{
+            System.out.println("等待客户端连接");
+            Socket socket = serverSocket.accept();//accept()方法会阻塞，直到有客户端连接
+            System.out.println("客户端连接成功");
+            InputStream in = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(in, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);
+
+            int d = in.read();
+            System.out.println(d);
+
+            while (true){
+                String str=br.readLine();
+                System.out.println(str);
+                if(str.equalsIgnoreCase("bye")){
+                    break;
+                }
+
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+
+    }
 }
